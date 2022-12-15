5e2042397b00b027e68575ac2af017ecd229702b
Mauricio <mauriciojs.junior@gmail.com>
2022-12-15 20:57:13 -0300

    Added url validator

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,5 +1,5 @@
+require 'uri'
 class Url < ApplicationRecord
   validates :original, :short, presence: true
-
-  # TODO: add validation original is a valid url
+  validates :original, url: true
 end
