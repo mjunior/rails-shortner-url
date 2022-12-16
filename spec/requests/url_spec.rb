c92d83592880ed409e62845df19f7f6955ee49df
Mauricio <mauriciojs.junior@gmail.com>
2022-12-15 23:55:37 -0300

    Increase unit test code coverage

--- spec/requests/url_spec.rb
+++ spec/requests/url_spec.rb
@@ -1,15 +1,36 @@
+require 'sidekiq/testing/inline'
 require 'rails_helper'
 
 RSpec.describe "Urls", type: :request do
-
+  let(:title_job) { double(TitleParserJob) }
 
   describe "POST /url" do
+    
+    before { allow(title_job).to receive(:perform_async).and_return(1) }
+
     it "returns http success" do
       allow_any_instance_of(CreateShortUrlService).to receive(:call).and_return("mockShortUrl")
 
       post "/url", params: { url: "https://google.com.br" }
 
       expect(JSON.parse(response.body)["short"]).to eq("mockShortUrl")
+    end
+
+    it "returns error if url is invalid" do
+      allow_any_instance_of(CreateShortUrlService).to receive(:call).and_return("mockShortUrl")
+
+      post "/url", params: { url: "googlecom.br" }
+      expect(JSON.parse(response.body)["url"][0]).to eq("is not an url. It should starts with https:// or http://")
+    end
+
+    it "should enque parse title job" do
+      allow_any_instance_of(CreateShortUrlService).to receive(:call).and_return("mockShortUrl")
+
+      Sidekiq::Testing.fake! { 
+        post "/url", params: { url: "https://google.com.br" }
+      }
+
+      expect(TitleParserJob.jobs.size).to eq(1)
     end
   end
 
