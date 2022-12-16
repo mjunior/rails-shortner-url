077fa03331fe961585bc780a73c3b38b60fb8c96
Mauricio <mauriciojs.junior@gmail.com>
2022-12-16 00:44:45 -0300

    added very simple handler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+  rescue_from StandardError, with: :basic_error_handler
+
+  def basic_error_handler(e)
+    # improve this adding specific message, for each expection
+    render json: { error: true, message: 'Internal Server Error' }, status: 500
+  end
 end
--- spec/models/url_spec.rb
+++ spec/models/url_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Url, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it 'invalid with empty original url' do
+      url = Url.new
+      url.valid?
+      expect(url.errors[:original]).to eq(['can\'t be blank'])
+    end
+
+    it 'invalid with wrong format' do
+      url = Url.new
+      url.original = "my-url-app"
+      url.valid?
+      expect(url.errors[:url]).to eq(['is not an url. It should starts with https:// or http://'])
+    end
+  end
 end
